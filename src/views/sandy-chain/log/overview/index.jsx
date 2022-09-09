bbc1f33880e46bfc3d97cb8b9dc25870a08d4fe5
jujubos <jujubos@163.com>
2022-09-09 11:11:14 +0800

    @ant-design upgrade to 4.0.0-rc.0

--- src/views/sandy-chain/log/overview/index.jsx
+++ src/views/sandy-chain/log/overview/index.jsx
@@ -1,12 +1,63 @@
-import React, { memo } from 'react'
+import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
+import { Card, Col, Row, Statistic } from 'antd';
+import React from 'react';
 
+const Overview = () => {
 
-const Overview = memo(() => {
+  const Style = {
+    padding: 30,
+    background: "#ececec",
+  }
+
   return (
-    <div>
-      asdasdasd
+    <div className="site-statistic-demo-card" style={Style}>
+      <Row gutter={200}>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Actiaaave"
+              value={11.28}
+              precision={2}
+              valueStyle={{
+                color: '#3f8600',
+              }}
+              prefix={<ArrowUpOutlined />}
+              suffix="%"
+              loading
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Idle"
+              value={9.3}
+              precision={2}
+              valueStyle={{
+                color: '#cf1322',
+              }}
+              prefix={<ArrowDownOutlined />}
+              suffix="%"
+            />
+          </Card>
+        </Col>
+        <Col span={8}>
+          <Card>
+            <Statistic
+              title="Idle"
+              value={9.3}
+              precision={2}
+              valueStyle={{
+                color: '#cf1322',
+              }}
+              prefix={<ArrowDownOutlined />}
+              suffix="%"
+            />
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
-})
+};
 
-export default Overview
+export default Overview;

4a14b8f170567b1ecdf3c32bd4d78fe198254a54
jujubos <jujubos@163.com>
2022-09-10 10:57:42 +0800

    333

--- src/views/sandy-chain/log/overview/index.jsx
+++ src/views/sandy-chain/log/overview/index.jsx
@@ -1,61 +1,19 @@
 import { ArrowDownOutlined, ArrowUpOutlined } from '@ant-design/icons';
 import { Card, Col, Row, Statistic } from 'antd';
 import React from 'react';
+import { PieChartTwoTone } from '@ant-design/icons'
+
+import Mixchart from './c-pages/Mixchart';
+import Panel from './c-pages/Panel'
 
 const Overview = () => {
 
-  const Style = {
-    padding: 30,
-    background: "#ececec",
-  }
-
   return (
-    <div className="site-statistic-demo-card" style={Style}>
-      <Row gutter={200}>
-        <Col span={8}>
-          <Card>
-            <Statistic
-              title="Actiaaave"
-              value={11.28}
-              precision={2}
-              valueStyle={{
-                color: '#3f8600',
-              }}
-              prefix={<ArrowUpOutlined />}
-              suffix="%"
-              loading
-            />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card>
-            <Statistic
-              title="Idle"
-              value={9.3}
-              precision={2}
-              valueStyle={{
-                color: '#cf1322',
-              }}
-              prefix={<ArrowDownOutlined />}
-              suffix="%"
-            />
-          </Card>
-        </Col>
-        <Col span={8}>
-          <Card>
-            <Statistic
-              title="Idle"
-              value={9.3}
-              precision={2}
-              valueStyle={{
-                color: '#cf1322',
-              }}
-              prefix={<ArrowDownOutlined />}
-              suffix="%"
-            />
-          </Card>
-        </Col>
-      </Row>
+    <div style={{padding: 20}}>
+      <Panel/>
+      <div>
+        <Mixchart/>
+      </div>
     </div>
   )
 };
